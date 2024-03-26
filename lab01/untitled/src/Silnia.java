dad2ff1c66e3a9b6d18fc2f968a4eb3172f7702b
matdomino <mdomino60@gmail.com>
2024-03-26 09:47:56 +0100

    Lab01 - liczenie silni

---
+++ lab01/untitled/src/Silnia.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Silnia {
+    static long silnia(int num) {
+        long result = 1;
+
+        for (int i = 1; i <= num; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        boolean status = true;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Witaj w programie do wyliczania silni!");
+        System.out.println("Wpisz \"exit\", aby wyjść z programu.");
+
+        while(status) {
+            System.out.print("Podaj liczbę: ");
+            String input = scanner.nextLine();
+
+            if (Objects.equals(input, "exit")){
+                status = false;
+            } else {
+                long result = silnia(Integer.parseInt(input));
+
+                System.out.println("Silnia z " + input + " to: " + result);
+            }
+        }
+    }
+}
